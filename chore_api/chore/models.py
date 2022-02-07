f28a83852d2fd288bd28315a2b7f7325a0e12c88
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 12:16:15 -0800

    made Household model

--- chore_api/chore/models.py
+++ chore_api/chore/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Household(models.Model):
+  name = models.CharField(max_length=100)
+  starter_list = models.BooleanField()
+  winner_board = models.BooleanField()
+
+  def __str__(self):
+      return self.name
+
