532ae194f2d9fc7aedb64dd65580a2825896bd58
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 12:38:14 -0800

    made User model

--- chore_api/chore/models.py
+++ chore_api/chore/models.py
@@ -3,9 +3,14 @@ from django.db import models
 # Create your models here.
 class Household(models.Model):
   name = models.CharField(max_length=100)
-  starter_list = models.BooleanField()
-  winner_board = models.BooleanField()
 
   def __str__(self):
       return self.name
 
+class User(models.Model):
+  household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='household')
+  username = models.CharField(max_length=100)
+  email = models.EmailField()
+  password_digest = models.CharField(max_length=100)
+  admin = models.BooleanField()
+  image = models.TextField()
