d2a5e767602692be30e6bbbf61f9229c3f449afc
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 15:29:32 -0800

    added blank=True to Chore notes and doer, tested successfully in browser GUI

--- chore_api/chore/models.py
+++ chore_api/chore/models.py
@@ -20,9 +20,9 @@ class User(models.Model):
 
 class Chore(models.Model):
   household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='chores')
-  doer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='chores')
+  doer = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='chores')
   name = models.CharField(max_length=100)
-  notes = models.TextField(null=True)
+  notes = models.TextField(null=True, blank=True)
   priority = models.PositiveSmallIntegerField()
   done = models.BooleanField()
 
