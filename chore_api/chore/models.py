463c991d9b311eb2c44c4bb69e1803df2dc27296
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 13:10:26 -0800

    made chore model and successfully migrated

--- chore_api/chore/models.py
+++ chore_api/chore/models.py
@@ -8,9 +8,23 @@ class Household(models.Model):
       return self.name
 
 class User(models.Model):
-  household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='household')
+  household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='users')
   username = models.CharField(max_length=100)
   email = models.EmailField()
   password_digest = models.CharField(max_length=100)
   admin = models.BooleanField()
   image = models.TextField()
+
+  def __str__(self):
+      return self.username 
+
+class Chore(models.Model):
+  household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='chores')
+  doer = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='chores')
+  name = models.CharField(max_length=100)
+  notes = models.TextField(null=True)
+  priority = models.PositiveSmallIntegerField()
+  done = models.BooleanField()
+
+  def __str__(self):
+    return self.name
