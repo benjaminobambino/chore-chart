a03283e96bfb017792a2c8eef55ee825615da625
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 11:36:11 -0800

    added .env for key

--- chore_api/chore_django/settings.py
+++ chore_api/chore_django/settings.py
@@ -20,8 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r8_go3r^_c&&ot4w$$whtqs7j4&+=pxt*v+20d0sh*jd(w%+16'
+from decouple import config
 
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
