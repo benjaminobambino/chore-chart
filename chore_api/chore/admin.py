f3d74cc505b8ccd7674f2fee2dbae4d3fd9d8f13
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 15:21:59 -0800

    registered models in admin.py

--- chore_api/chore/admin.py
+++ chore_api/chore/admin.py
@@ -1,3 +1,8 @@
+import imp
 from django.contrib import admin
+from .models import Household, User, Chore
 
 # Register your models here.
+admin.site.register(Household)
+admin.site.register(User)
+admin.site.register(Chore)
