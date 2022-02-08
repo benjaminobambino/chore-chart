7f36ecf281b5eb03ba8ae6a3e9c2ffc0ec040e23
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-08 10:02:41 -0800

    added more CRUD reducer cases to HouseholdReducer, added types for all three models

--- client/src/store/reducers/HouseholdReducer.js
+++ client/src/store/reducers/HouseholdReducer.js
@@ -1,12 +1,16 @@
 const {
   GET_CURRENT_HOUSEHOLD,
   NEW_HOUSEHOLD,
-  ADD_HOUSEHOLD
+  ADD_HOUSEHOLD,
+  EDIT_HOUSEHOLD,
+  UPDATE_HOUSEHOLD,
+  DELETE_HOUSEHOLD
 } = require('../types');
 
 const iState = {
   currentHousehold: {},
-  newHousehold: {}
+  newHousehold: {},
+  editedHousehold: {}
 };
 
 const HouseholdReducer = (state = iState, action) => {
@@ -17,6 +21,12 @@ const HouseholdReducer = (state = iState, action) => {
       return { ...state, newHousehold: action.payload };
     case ADD_HOUSEHOLD:
       return { ...state, currentHousehold: action.payload };
+    case EDIT_HOUSEHOLD:
+      return { ...state, editedHousehold: action.payload };
+    case UPDATE_HOUSEHOLD:
+      return { ...state, currentHousehold: action.payload };
+    case DELETE_HOUSEHOLD:
+      return { ...state, currentHousehold: {} };
     default:
       return { ...state };
   }
--- client/src/store/types.js
+++ client/src/store/types.js
@@ -1,3 +1,18 @@
 export const GET_CURRENT_HOUSEHOLD = 'GET_CURRENT_HOUSEHOLD';
 export const NEW_HOUSEHOLD = 'NEW_HOUSEHOLD';
 export const ADD_HOUSEHOLD = 'ADD_HOUSEHOLD';
+export const EDIT_HOUSEHOLD = 'EDIT_HOUSEHOLD';
+export const UPDATE_HOUSEHOLD = 'UPDATE_HOUSEHOLD';
+export const DELETE_HOUSEHOLD = 'DELETE_HOUSEHOLD';
+
+export const GET_USERS = 'GET_USERS';
+export const EDIT_USER = 'EDIT_USER';
+export const UPDATE_USER = 'UPDATE_USER';
+export const DELETE_USER = 'DELETE_USER';
+
+export const GET_CHORES = 'GET_CHORES';
+export const NEW_CHORE = 'NEW_CHORE';
+export const ADD_CHORE = 'ADD_CHORE';
+export const EDIT_CHORE = 'EDIT_CHORE';
+export const UPDATE_CHORE = 'UPDATE_CHORE';
+export const DELETE_CHORE = 'DELETE_CHORE';
