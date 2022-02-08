ba683158c8aa7f1379c776f396da534b28970c9f
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-07 18:23:07 -0800

    created list and detail view for each model

--- chore_api/chore/views.py
+++ chore_api/chore/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .serializers import HouseholdSerializer, UserSerializer, ChoreSerializer
+from .models import Household, User, Chore
 
 # Create your views here.
+class HouseholdList(generics.ListCreateAPIView):
+  queryset = Household.objects.all()
+  serializer_class = HouseholdSerializer
+
+class HouseholdDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Household.objects.all()
+  serializer_class = HouseholdSerializer
+
+class UserList(generics.ListCreateAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = User.objects.all()
+  serializer_class = UserSerializer
+
+class ChoreList(generics.ListCreateAPIView):
+  queryset = Chore.objects.all()
+  serializer_class = ChoreSerializer
+
+class ChoreDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Chore.objects.all()
+  serializer_class = ChoreSerializer
