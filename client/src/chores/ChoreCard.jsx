155d862551b7a7fade47bb01649075b0ea99f6d4
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-09 11:28:32 -0800

    added markComplete and deleteChore functionality

--- client/src/chores/ChoreCard.jsx
+++ client/src/chores/ChoreCard.jsx
@@ -3,12 +3,13 @@ import React from "react";
 
 const ChoreCard = ({ chore, user, getHousehold }) => {
   const BASE_URL = process.env.REACT_APP_BASE_URL
-  // change auth creds once auth is implemented
+  // change creds once auth is implemented
   const authUser = process.env.REACT_APP_USERNAME
   const authPassword = process.env.REACT_APP_PASSWORD
 
   const claimed = chore.doer === null ? false : true
-  const mine = chore.doer_id === user.id ? true : false  
+  const mine = chore.doer_id === user.id ? true : false
+  const complete = chore.done ? true : false  
   
   const priority = chore.priority
   let priorityMessage
@@ -50,13 +51,61 @@ const ChoreCard = ({ chore, user, getHousehold }) => {
     }
   }
 
+  const markComplete = async (choreId) => {
+    if (!complete) {
+      await axios
+        .put(`${BASE_URL}/chores/${choreId}`,
+          { ...chore, done: true },
+          {
+            auth: {
+              username: authUser,
+              password: authPassword
+            }
+          })
+        .then(() => {
+          getHousehold()
+        })
+    } else {
+      await axios
+        .put(`${BASE_URL}/chores/${choreId}`,
+          { ...chore, done: false },
+          {
+            auth: {
+              username: authUser,
+              password: authPassword
+            }
+          })
+          .then(() => {
+            getHousehold()
+        })
+    }
+  }
+
+  const deleteChore = async (choreId) => {
+    if (user.admin) {
+      await axios
+        .delete(`${BASE_URL}/chores/${choreId}`,
+          {
+            auth: {
+              username: authUser,
+              password: authPassword
+            }
+          })
+        .then(() => {
+          getHousehold()
+        })
+    }
+  }
+
   return (
     <div>
       <h4>{chore.name}</h4>
+      {/* checkbox version */ mine ? <input type="checkbox" checked={complete ? true : false} onChange={()=> {markComplete(chore.id)}} /> : null }
+      {/* button version { mine ? <button className="checkbox" onClick={()=> {markComplete(chore.id)}}>{complete ? <h4>&#10003;</h4> : null }</button> : null } */}
       <p>{chore.notes}</p>
       <h5>{priorityMessage}</h5>
       <button onClick={() => {claimChore(chore.id)}}>{ !claimed ? 'Claim' : 'Unclaim' } </button>
-      { mine ? <button>Mark Complete</button> : null }
+      {user.admin ? <button onClick={() => {deleteChore(chore.id)}}>Delete</button> : null }
     </div>
   )
 }
