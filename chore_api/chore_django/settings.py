8666142b8404c83f3c4bb36b0dc117aa1b59c642
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-10 08:44:16 -0800

    changed permission classes and added auth classes in settings.py

--- chore_api/chore_django/settings.py
+++ chore_api/chore_django/settings.py
@@ -52,8 +52,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         # 'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
         # ???
-        'rest_framework.permissions.IsAuthenticated'
-    ]
+        # 'rest_framework.permissions.IsAuthenticated'
+        # ???
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
 }
 
 MIDDLEWARE = [
