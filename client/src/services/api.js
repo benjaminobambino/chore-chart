6210df9321fbc22397e070e8484eae262ce2d58a
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-10 20:38:01 -0800

    added two-thirds of Auth functions (CheckSession and RegisterUser)

---
+++ client/src/services/api.js
@@ -0,0 +1,16 @@
+import Axios from 'axios';
+
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
+const Client = Axios.create({ baseURL: BASE_URL });
+Client.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+export default Client;
