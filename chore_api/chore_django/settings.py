b8a7be61e4a61df52301898cad0bbe4e8ed87ed3
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-11 16:28:28 -0800

    lengthened access token lifetime

--- chore_api/chore_django/settings.py
+++ chore_api/chore_django/settings.py
@@ -193,8 +193,8 @@ from datetime import timedelta
 ...
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
