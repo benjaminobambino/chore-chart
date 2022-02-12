69fa8362d343087156295e739d01ed589e60bb35
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-11 23:42:31 -0800

    changed token access lifetime back to five minutes after solving refresh puzzle

--- chore_api/chore_django/settings.py
+++ chore_api/chore_django/settings.py
@@ -193,7 +193,7 @@ from datetime import timedelta
 ...
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
