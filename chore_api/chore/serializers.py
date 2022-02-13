086adaa02c354a4c8b1cafbff9fb93f606e19f85
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-12 16:21:48 -0800

    swapped chore and user serializer orders to test for potential nested serializer

--- chore_api/chore/serializers.py
+++ chore_api/chore/serializers.py
@@ -3,38 +3,6 @@ from rest_framework import serializers
 from .models import Household, Chore
 from accounts.models import CustomUser
 
-class ChoreSerializer(serializers.HyperlinkedModelSerializer):
-  household = serializers.HyperlinkedRelatedField(
-    view_name = 'household_detail',
-    read_only = True
-  )
-
-  household_id = serializers.PrimaryKeyRelatedField(
-    queryset = Household.objects.all(),
-    source = 'household'
-  )
-
-  doer = serializers.HyperlinkedRelatedField(
-    required = False,
-    view_name = 'user_detail',
-    read_only = True,
-  )
-
-  doer_id = serializers.PrimaryKeyRelatedField(
-    required = False,
-    allow_null = True,
-    queryset = CustomUser.objects.all(),
-    source = 'doer'
-  )
-
-  chore_url = serializers.ModelSerializer.serializer_url_field(
-    view_name = 'chore_detail'
-  )
-
-  class Meta:
-    model = Chore
-    fields = ('id', 'name', 'notes', 'priority', 'done', 'household', 'household_id', 'doer', 'doer_id', 'chore_url')
-
 # class CustomUserSerializer(serializers.ModelSerializer):
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
@@ -81,6 +49,40 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
   # class Meta:
   #   model = CustomUser
   #   fields = ('id', 'username', 'email', 'admin', 'image', 'user_url', 'household', 'household_id', 'chores')
+
+class ChoreSerializer(serializers.HyperlinkedModelSerializer):
+  household = serializers.HyperlinkedRelatedField(
+    view_name = 'household_detail',
+    read_only = True
+  )
+
+  household_id = serializers.PrimaryKeyRelatedField(
+    queryset = Household.objects.all(),
+    source = 'household'
+  )
+
+  doer = serializers.HyperlinkedRelatedField(
+    required = False,
+    view_name = 'user_detail',
+    read_only = True,
+  )
+
+  doer_id = serializers.PrimaryKeyRelatedField(
+    required = False,
+    allow_null = True,
+    queryset = CustomUser.objects.all(),
+    source = 'doer'
+  )
+
+  chore_url = serializers.ModelSerializer.serializer_url_field(
+    view_name = 'chore_detail'
+  )
+
+  class Meta:
+    model = Chore
+    fields = ('id', 'name', 'notes', 'priority', 'done', 'household', 'household_id', 'doer', 'doer_id', 'chore_url')
+
+
 
 class HouseholdSerializer(serializers.HyperlinkedModelSerializer):
   users = UserSerializer(
