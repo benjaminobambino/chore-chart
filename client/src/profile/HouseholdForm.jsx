18165c458f9d02bc6b7832bbf1c4e156a56478d5
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-12 23:24:30 -0800

    set up filter and displayedMessage in HouseholdForm

--- client/src/profile/HouseholdForm.jsx
+++ client/src/profile/HouseholdForm.jsx
@@ -5,16 +5,18 @@ const HouseholdForm = (props) => {
   const [householdOptions, setHouseholdOptions] = useState([])
   const [inputValue, setInputValue] = useState({
     name: '',
-    household_id: null
+    household_id: null,
+    admin: false
   })
   const [addNew, setAddNew] = useState(null)
+  const [displayedMessage, setDisplayedMessage] = useState('')
 
   const handleChange = (e) => {
     setInputValue({ ...inputValue, [e.target.name]: e.target.value });
   };
 
   const assignHousehold = async (householdId) => {
-    await Client.patch(`/users/${props.user.id}`, { ...props.user, household_id: householdId}, {
+    await Client.patch(`/users/${props.user.id}`, { ...props.user, household_id: householdId, admin: inputValue.admin}, {
     }).then(() => {
       props.getUser(props.user.id)
       props.history.push('/chores')
@@ -30,7 +32,11 @@ const HouseholdForm = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault()
     if (addNew) {
-      addHousehold()
+      if (!inputValue.name.length) {
+        setDisplayedMessage('Your household must have a name.')
+      } else {
+        addHousehold()
+      }
     } else if (addNew === false) {
       assignHousehold(inputValue.household_id)
     }
@@ -42,16 +48,24 @@ const HouseholdForm = (props) => {
         return a.id - b.id
       })
       setHouseholdOptions(sortById)
-      setInputValue({ ...inputValue, household_id: 1})
     })
   }
 
+  const handleNewOption = () => {
+    setAddNew(true)
+    setInputValue({ ...inputValue, admin: true, household_id: null })
+  }
+
   const handleExistingOption = () => {
-    getHouseholds()
     setAddNew(false)
+    setInputValue({ ...inputValue, admin: false, household_id: 1})
+    getHouseholds()
   }
 
   useEffect(() => {
+    if (props.user.household_id) {
+      props.history.push('/')
+    }
     getHouseholds()
   }, [])
 
@@ -61,7 +75,7 @@ const HouseholdForm = (props) => {
       <p>Every Chore Chart user must be part of a household. Would you like to join an existing household or create a new one?</p>
       <form className="new-or-existing-household">
         <label htmlFor="new">Make a New Household</label>
-        <input type="radio" id="new" name="new-or-existing" onChange={() => setAddNew(true)} />
+        <input type="radio" id="new" name="new-or-existing" onChange={handleNewOption} />
         <label htmlFor="existing">Join an Existing Household</label>
         <input type="radio" id="existing" name="new-or-existing" onChange={handleExistingOption} />
       </form>
@@ -70,6 +84,7 @@ const HouseholdForm = (props) => {
         <label htmlFor="name">Name</label>
         <input type="text" name="name" onChange={handleChange} />
         <button type="submit">Add Household</button>
+        <p>{displayedMessage}</p>
       </form>
         : null}
       {addNew === false ?
--- client/src/profile/ProfileCard.jsx
+++ client/src/profile/ProfileCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const ProfileCard = ({ user }) => {
   return (
     <div>
-      <h3>{user.username}</h3>
+      <h3>{user.username} {user.admin ? '(admin)' : null}</h3>
     </div>
   )
 }
