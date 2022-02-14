d7ee4d8fbf965a29be119a8094ba84b924ffef96
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-13 23:46:14 -0800

    sized and aligned icon and title

--- client/src/misc/Header.jsx
+++ client/src/misc/Header.jsx
@@ -5,9 +5,12 @@ import PublicNavBar from "../nav/PublicNavBar";
 const Header = ({ authenticated, user, handleLogOut }) => {
   return (
     <div className="header">
-      <section className="title">
+      <section className="title-container">
         <img src="./checklist-icon.png" alt="chore chart" className="icon"/>
-        <h1 className="header">Chore Chart</h1>
+        <section className="title">
+          <h1>Chore</h1>
+          <h1>Chart</h1>
+        </section>
       </section>
       { authenticated && user ? 
         <UserNavBar handleLogOut={handleLogOut} />
