c8e067e764ddfeda429d4ad8a8e6e467100a8e82
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-13 16:38:37 -0800

    Household pushes to login if no token

--- client/src/profile/Household.jsx
+++ client/src/profile/Household.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProfileCard from "./ProfileCard";
 
-const Household = ({ users, name }) => {
-  const adminFirst = users.sort((a, b) => {
-    return b.admin - a.admin
-  })
+const Household = (props) => {
+  // useEffect(() => {
+    let adminFirst
+    if (!localStorage.getItem('token')) {
+      props.history.push('/login')
+    } else {
+      adminFirst = props.users.sort((a, b) => {
+        return b.admin - a.admin
+      })          
+    }
+  // })
+
+
+  
 
   return (
     <div>
-      <h2>{name} Team Members</h2>
-      {users &&
+      <h2>{props.name} Team Members</h2>
+      {props.users &&
       adminFirst.map((user) => {
         return(
-          <div key={user.id}>
-            <ProfileCard user={user} />
+          <div key={props.user.id}>
+            <ProfileCard user={props.user} />
           </div>
         )
       })}
