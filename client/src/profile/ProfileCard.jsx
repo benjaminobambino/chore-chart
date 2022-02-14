6d4ef535c0fc343b2cd7b725c20d32d7e9aa630e
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-14 01:52:47 -0800

    added admin icon and make admin button and functionality

--- client/src/profile/ProfileCard.jsx
+++ client/src/profile/ProfileCard.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Client from "../services/api";
 
-const ProfileCard = ({ user }) => {
+const ProfileCard = ({ user, currentUser, getHousehold, household }) => {
+  const makeAdmin = async (userId) => {
+    await Client.patch(`/users/${userId}`, {admin: true}).then(() => {
+      getHousehold(household.id)
+    })
+  }
+
+  const confirmAdmin = () => {
+    const confirm = window.confirm(`Are you sure you want to make ${user.name} a household admin?`)
+    if (confirm) {
+      makeAdmin(user.id)
+    }
+  }
+
   return (
-    <div>
-      <h3>{user.username} {user.admin ? '(admin)' : null}</h3>
+    <div className="profile-card">
+      {user.admin ? <img src="checklist-icon.png" alt="admin" className="admin-icon"/> : null}
+      <h3>{user.username}</h3>
+      {currentUser.admin && currentUser.id !== user.id && !user.admin ? <button onClick={confirmAdmin}>Make admin</button> : null}
     </div>
   )
 }
