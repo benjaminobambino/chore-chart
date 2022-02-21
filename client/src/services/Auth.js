2e357fb9514608fb0454b6da40d912806798f0ef
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-02-21 14:35:44 -0800

    cleaned up commented out lines

--- client/src/services/Auth.js
+++ client/src/services/Auth.js
@@ -1,26 +1,5 @@
 import Client from './api';
 
-// export const LogInUser = async (data) => {
-//   try {
-//     const res = await Client.post('/api/token/', data);
-//     localStorage.setItem('refresh', res.data.refresh);
-//     localStorage.setItem('token', res.data.access);
-//     return res.data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
-// export const RegisterUser = async (data) => {
-//   try {
-//     const res = await Client.post('/api/user/signup/', data);
-//     return res.data;
-//   } catch (error) {
-//     alert('Name and/or email already taken.');
-//     throw error;
-//   }
-// };
-
 export const CheckSession = async () => {
   try {
     const refresh = localStorage.getItem('refresh');
