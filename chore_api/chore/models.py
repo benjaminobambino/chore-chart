d02bfe7ea5fe33b577aa1d6813dff67ee09a19f8
Benjamin Peck <benjaminlpeck@gmail.com>
2022-03-12 17:53:46 -0800

    deleted old user model commented out code

--- chore_api/chore/models.py
+++ chore_api/chore/models.py
@@ -11,35 +11,6 @@ class Household(models.Model):
   def __str__(self):
       return self.name
 
-# class User(models.Model):
-#   household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='users')
-#   username = models.CharField(max_length=100)
-#   email = models.EmailField(unique=True)
-#   admin = models.BooleanField()
-#   image = models.TextField()
-
-#   def __str__(self):
-#       return self.username 
-
-# class User(AbstractBaseUser, PermissionsMixin):
-#   email = models.EmailField(unique=True)
-#   username = models.CharField(max_length=150, unique=True)
-#   household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='users')
-#   admin = models.BooleanField()
-#   image = models.TextField()
-
-#   def __str__(self):
-#       return self.username 
-
-#   is_staff = models.BooleanField(default=False)
-#   is_active = models.BooleanField(default=True)
-
-#   USERNAME_FIELD = 'email'
-#   REQUIRED_FIELDS = ['username', 'household']
-
-#   def __str__(self):
-#       return self.username
-
 class Chore(models.Model):
   household = models.ForeignKey(Household, on_delete=models.CASCADE, related_name='chores')
   doer = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.SET_NULL, related_name='chores')
